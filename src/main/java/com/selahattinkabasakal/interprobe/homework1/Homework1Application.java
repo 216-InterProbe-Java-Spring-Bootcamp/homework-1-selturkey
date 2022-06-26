9c2c942e8e77f7d9e57de066224099fd2a60fd30
selturkey <selturkey@gmail.com>
2022-06-26 15:49:18 +0300

    Initialize Spring Boot and Configure application.properties and maven

---
+++ src/main/java/com/selahattinkabasakal/interprobe/homework1/Homework1Application.java
@@ -0,0 +1,13 @@
+package com.selahattinkabasakal.interprobe.homework1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework1Application.class, args);
+    }
+
+}
